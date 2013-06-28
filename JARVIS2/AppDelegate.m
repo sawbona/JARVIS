e1e0afa20cd1052e9426a196bf240cea8eff5630
Héctor Alonso Guzmán Gutiérrez <hguzman.sistemas@gmail.com>
2013-06-28 10:03:22 -0500

    Prueba un espacio

--- JARVIS2/AppDelegate.m
+++ JARVIS2/AppDelegate.m
@@ -3,8 +3,8 @@
 //  JARVIS2
 //
 //  Created by Francisco Esteban Aguilar on 03/05/13.
-//  Copyright (c) 2013 Francisco Esteban Aguilar. All rights reserved.
-//
+//  Copyright (c) 2013 Francisco Esteban Aguilar. All rights reserved .
+// 
 
 #import "AppDelegate.h"
 #import "FirstViewController.h"
